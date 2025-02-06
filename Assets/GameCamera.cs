517b613df086b0e33778235e912dcd29136448ee
Devin Irwin <Irwin-D18@ulster.ac.uk>
2025-02-06 14:52:03 +0000

    meow

--- Assets/GameCamera.cs
+++ Assets/GameCamera.cs
@@ -9,12 +9,12 @@ public class GameCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, transform.position.y, -10), 0.05f);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, transform.position.y, transform.position.z), 0.05f);
     }
 }
